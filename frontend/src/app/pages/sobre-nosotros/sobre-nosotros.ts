ec9e509bf6865fb5c74af013f4f7a76d268bc103
Brayan Villa <brayanvillla@gmail.com>
2026-07-21 22:04:52 -0500

    Entrega 6: perfil de usuario y pagina sobre nosotros

--- frontend/src/app/pages/sobre-nosotros/sobre-nosotros.ts
+++ frontend/src/app/pages/sobre-nosotros/sobre-nosotros.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sobre-nosotros',
-  imports: [],
   templateUrl: './sobre-nosotros.html',
   styleUrl: './sobre-nosotros.css',
 })
